7122bce1ae11123a3bfa68fdf854574b0c6e80cd
piaofu123 <tfgk123@163.com>
2018-11-22 20:24:41 +0800

    update file

--- src/main/java/com/trend/TrendMonitorApplication.java
+++ src/main/java/com/trend/TrendMonitorApplication.java
@@ -2,8 +2,6 @@ package com.trend;
 
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
